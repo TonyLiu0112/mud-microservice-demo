552da0b53d1d42e795ceb769f53b6497642043ca
tonyLiu0112 <tony.liu0112@gmail.com>
2017-04-11 10:31:59 +0800

    子项目说明文件

--- mud-microservice-security/src/main/java/com/tony/demo/microservice/mud/SecurityApplication.java
+++ mud-microservice-security/src/main/java/com/tony/demo/microservice/mud/SecurityApplication.java
@@ -11,6 +11,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.util.ListUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -35,4 +42,5 @@ public class SecurityApplication {
     RequestDumperFilter requestDumperFilter() {
         return new RequestDumperFilter();
     }
+
 }
