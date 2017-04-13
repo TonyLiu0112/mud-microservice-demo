d4677c9d0dc2dc14bd2c2b6a8fed2c335805dc96
tonyLiu0112 <tony.liu0112@gmail.com>
2017-04-13 10:14:58 +0800

    sso 集群模式

--- mud-microservice-security/src/main/java/com/tony/demo/microservice/mud/SecurityApplication.java
+++ mud-microservice-security/src/main/java/com/tony/demo/microservice/mud/SecurityApplication.java
@@ -11,13 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.thymeleaf.util.ListUtils;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableEurekaClient
--- mud-microservice-web-manager/src/main/java/com/tony/demo/microservice/mud/web/manager/conf/OAuth2Config.java
+++ mud-microservice-web-manager/src/main/java/com/tony/demo/microservice/mud/web/manager/conf/OAuth2Config.java
@@ -1,29 +1,14 @@
 package com.tony.demo.microservice.mud.web.manager.conf;
 
-import org.apache.catalina.filters.RequestDumperFilter;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 
 @Configuration
-@EnableOAuth2Sso
 public class OAuth2Config {
-
-    /**
-     * 请求dump过滤器，用于查看所有请求的请求头信息
-     *
-     * @return
-     */
-    @Profile("!pro") // 如果不是生产环境的profile，则打开此配置
-    @Bean
-    RequestDumperFilter requestDumperFilter() {
-        return new RequestDumperFilter();
-    }
 
     @Bean
     @LoadBalanced
