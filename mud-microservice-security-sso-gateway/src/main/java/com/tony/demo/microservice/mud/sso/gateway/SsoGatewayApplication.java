a0b2a67669ed7ec72c1204529b287565dcf6c31d
tonyLiu0112 <tony.liu0112@gmail.com>
2017-04-26 18:16:26 +0800

    重构

--- mud-microservice-security-sso-gateway/src/main/java/com/tony/demo/microservice/mud/sso/gateway/SsoGatewayApplication.java
+++ mud-microservice-security-sso-gateway/src/main/java/com/tony/demo/microservice/mud/sso/gateway/SsoGatewayApplication.java
@@ -1,10 +1,9 @@
-package com.tony.demo.microservice.mud;
+package com.tony.demo.microservice.mud.sso.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableEurekaClient
