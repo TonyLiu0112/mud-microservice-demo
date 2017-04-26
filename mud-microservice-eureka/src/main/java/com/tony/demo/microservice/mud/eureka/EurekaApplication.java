84deec5016484c9b57e9bf89d45028c648209437
tonyLiu0112 <tony.liu0112@gmail.com>
2017-04-26 18:01:33 +0800

    重构

--- mud-microservice-eureka/src/main/java/com/tony/demo/microservice/mud/eureka/EurekaApplication.java
+++ mud-microservice-eureka/src/main/java/com/tony/demo/microservice/mud/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.tony.demo.microservice.mud;
+package com.tony.demo.microservice.mud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
