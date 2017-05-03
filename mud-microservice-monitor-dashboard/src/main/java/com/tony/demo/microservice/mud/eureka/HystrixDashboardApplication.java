f31a32cd305bff7b9bfce51e82d89c8c7aa3495b
tonyLiu0112 <tony.liu0112@gmail.com>
2017-05-03 21:28:46 +0800

    监控 hystrix dashboard

---
+++ mud-microservice-monitor-dashboard/src/main/java/com/tony/demo/microservice/mud/eureka/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.tony.demo.microservice.mud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
