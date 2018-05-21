9ee80b7b2d712e0351eff977a98dac55b2707f62
tonyLiu0112 <tony.liu0112@gmail.com>
2018-05-21 09:47:21 +0800

    优化

--- mud-microservice-services/ms-order/src/main/kotlin/com/tony/demo/microservice/mud/services/order/eventuate/command/CreateOrderCommand.kt
+++ mud-microservice-services/ms-order/src/main/kotlin/com/tony/demo/microservice/mud/services/order/eventuate/command/CreateOrderCommand.kt
@@ -3,14 +3,8 @@ package com.tony.demo.microservice.mud.services.order.eventuate.command
 import com.tony.demo.microservice.mud.common.eventuate.domain.Money
 
 
-class CreateOrderCommand : OrderCommand {
-    var customerId : String? = null
-    var money : Money? = null
+class CreateOrderCommand(customerId: String, money: Money) : OrderCommand {
+    var customerId : String? = customerId
+    var money : Money? = money
 
-    constructor()
-
-    constructor(customerId: String, money : Money) {
-        this.customerId = customerId
-        this.money = money
-    }
 }
